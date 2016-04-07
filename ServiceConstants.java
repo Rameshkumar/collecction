6de6fe827ddb59e64c868cdb928c5affdb99de72
Rameshkumar <ramesh.ramasamy@gmail.com>
2016-04-06 21:51:03 -0500

    Create ServiceConstants.java

---
+++ ServiceConstants.java
@@ -0,0 +1,70 @@
+/**
+ * @author RAMESHKUMAR
+ *
+ */
+public class ServiceConstants {
+	
+	/**
+	 * Prevents instantiation
+	 */
+	private ServiceConstants() {
+	}
+
+	public static final String STATUS_SUCCESS_CODE = "01";
+	public static final String STATUS_ERROR_CODE = "-1";
+	public static final String STATUS_FAILURE_CODE = "02";
+
+	public static final String STATUS_SUCCESS_MSG = "Success";
+	public static final String STATUS_ERROR_MSG = "Error";
+	public static final String STATUS_FAILURE_MSG = "Failure";
+	
+	public static final String MPID_DESIGNATOR = "MPID";
+	
+	public static final String DATE_FORMAT = "MM/dd/yyyy";
+	
+	public static final String MONTH_FORMAT = "MMM";
+	
+	public static final String DB_DATE_FORMAT = "MM-dd-yyyy";
+	
+	public static final int DEFAULT_NUMBER_OF_DECIMALS = 2;
+	
+	public static final String APP_CONFIG_VALUES_DELIMITER = ";";
+	
+	public static final Integer APP_CONFIG_STATUS_CD = 4;
+	
+	public static final Integer CRITICAL_CD = 0; 
+	
+	public static final Integer NORMAL_CD = 1;
+	
+	public static final Integer APP_CONFIG_PRIORITY_CD = 3;
+	
+	public static final Integer PENDING_CD = 0;
+	
+	public static final Integer AWAITING_FOLLOW_UP_CD = 1;
+	
+	public static final Integer RESOLVED_CD = 2;
+
+	public static final Integer APP_CONFIG_REVIEWER_RATINGS_CD = 2;
+
+	public static final Integer APP_CONFIG_DECLINED_TYPES_CD = 1;
+	
+	public static final Integer APP_CONFIG_APPROVED_TYPES_CD = 0;
+	
+	public static final String HOME_PHONE = "homePhone";
+	
+	public static final String WORK_PHONE = "workPhone";
+	
+	public static final String MOBILE_PHONE = "mobilePhone";
+	
+	public static final String HOME_FAX = "homeFax";
+	
+	public static final String WORK_FAX = "workFax";
+	
+	public static final String PHONE = "Phone";
+	
+	public static final String FAX = "Fax";
+	
+	public static final String MAIL = "Mail";
+
+
+}
